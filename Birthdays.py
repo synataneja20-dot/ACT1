9d9b31ee5030114647e550ad60e2177ae066d8bc
synataneja20-dot <synataneja20@gmail.com>
2025-11-08 15:08:24 -0600

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,2 @@
+print("Raj's Birthday is on the 10th, ", "Emily's's Birthday is on the 16th, ", "Tom's's Birthday is on the 21st, ", "Gabe's bithday is on the 30th")
+print("Raj's other friends refused to tell him when thier birthdays were")
